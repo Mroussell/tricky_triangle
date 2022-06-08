a52a28443e976ba0745c8fda5cd4eb52c9138cd8
Michael Roussell <55032397+Mroussell@users.noreply.github.com>
2022-06-08 15:48:04 -0400

    init commit

---
+++ components/GameBoard.js
@@ -0,0 +1,12 @@
+import react from "react";
+import styles from "../styles/GameBoard.module.css"
+
+export default function GameBoard() {
+    return(
+        <>
+            <div className={styles.board}>
+                
+            </div>
+        </> 
+    )
+  };
